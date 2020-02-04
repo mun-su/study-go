bdd5221c47f9f23831ef68962f3f65ec026d7882
mun-su <dev.munsu@gmail.com>
2020-02-04 18:10:24 +0900

    숫자 연산

---
+++ src/section4/number-op/number-op-1.go
@@ -0,0 +1,18 @@
+package main
+
+import "math"
+
+func main() {
+	// 숫자 연산(산술, 비교)
+	// 타입이 같아야 가능. (정수, 실수) 엄격함.
+	// 다른 타입끼리는 반드시 형 변환 후 연산
+	// 형 변환 없을 경우 예외(에러) 발생
+	// +, -, *, %, /, <<, >>, &, ^
+
+	// ex 1
+	var n1 uint8 = math.MaxFloat32
+	var n2 uint8 = math.MaxFloat32
+	var n3 uint8 = math.MaxFloat32
+	var n4 uint8 = math.MaxFloat32
+
+}
