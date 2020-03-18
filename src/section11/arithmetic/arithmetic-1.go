0f6876a50ab803478d641c1a158f05024d3ef33f
mun-su <dev.munsu@gmail.com>
2020-03-19 02:17:14 +0900

    go 사용자 패키지 제작 및 문서화

--- src/section11/arithmetic/arithmetic-1.go
+++ src/section11/arithmetic/arithmetic-1.go
@@ -7,18 +7,22 @@ type Numbers struct {
 	Y int
 }
 
+// 덧셈
 func (o *Numbers) Addition() int {
 	return o.X + o.Y
 }
 
+// 뺄셈
 func (o *Numbers) Subtraction() int {
 	return o.X - o.Y
 }
 
+// 곱셈
 func (o *Numbers) Multiply() int {
 	return o.X * o.Y
 }
 
+// 나눗셈
 func (o *Numbers) Division() int {
 	return o.X / o.Y
 }
